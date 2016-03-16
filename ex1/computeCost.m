8e8db1ecc491701be44590d1e502755c40339bb2
dougwells <dougwells@gmail.com>
2016-03-16 15:39:35 -0600

    Cost function working.  Moving to updating thetas

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -1,19 +1,25 @@
 function J = computeCost(X, y, theta)
-%COMPUTECOST Compute cost for linear regression
+% Doug notes: ComputeCost is a function that is used in ex1.m
+% COMPUTECOST Compute cost for linear regression
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
+% Doug: line below says to make final solution = J.  J=sumOfSquares/2m
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-J = (2*m)^(-1)*theta'*X;
+m = length(X);
+pred = X*theta;
+act = y;
+errorSquared = (pred-act).^2;	% square each element in errorSquared array
+J = sum(errorSquared)/(2*m);	%sum all elements in array & divide by 2m
 
 
 
--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -6,8 +6,14 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+    
+% Calculate predicted values with initial theta
+pred=X*theta;
 
-for iter = 1:num_iters
+% Just renaming y vector to make descriptive
+act=y;
+
+for count = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -16,17 +22,36 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    % run gradient descent
+    % theta = gradientDescent(X, y, theta, alpha, iterations);
+    % m = length(X);
+    % pred = X*theta;
+    % act = y;
+    % errorSquared = (pred-act).^2;     % square each element in errorSquared array
+    % J = sum(errorSquared)/(2*m);      %sum all elements in array & divide by 2m
 
+% Adjust theta
+    error=pred-act;
+    sumErrX0 = sum(error.*X(:,1));       %sum element multip. of error times Xs first col
+    sumErrX1 = sum(error.*X(:,2));       %sum element multip of error times Xs second column
+    adjust0 = sumErrX0*(alpha/m);
+    adjust1 = sumErrX1*(alpha/m);
+    fprintf('%f %f \n', adjust0, adjust1);
+    pause;
 
-
-
+% Calculate J with new values of theta    
+    errorSquared=error.^2;
+    J = sum(errorSquared)/(2*m);
+% print theta to screen
+fprintf('Theta found by gradient descent: ');
+fprintf('%f %f \n', theta(1), theta(2));
 
 
 
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
+    J_history(count) = computeCost(X, y, theta);
 
 end
 
