e09045e0a9f5d862230d7a523a8422101469d05d
dougwells <dougwells@gmail.com>
2016-03-27 12:10:44 -0600

    Wk 4: Final Commit.  Neural Network Prediction Function.  Use NN to predict value of handwritten numbers

--- ex3/ex3_nn.m
+++ ex3/ex3_nn.m
@@ -2,9 +2,9 @@
 
 %  Instructions
 %  ------------
-% 
+%
 %  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
+%  linear exercise. You will need to complete the following functions
 %  in this exericse:
 %
 %     lrCostFunction.m (logistic regression cost function)
@@ -22,11 +22,11 @@ clear ; close all; clc
 %% Setup the parameters you will use for this exercise
 input_layer_size  = 400;  % 20x20 Input Images of Digits
 hidden_layer_size = 25;   % 25 hidden units
-num_labels = 10;          % 10 labels, from 1 to 10   
+num_labels = 10;          % 10 labels, from 1 to 10
                           % (note that we have mapped "0" to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset. 
+%  We start the exercise by first loading and visualizing the dataset.
 %  You will be working with a dataset that contains handwritten digits.
 %
 
@@ -37,16 +37,20 @@ load('ex3data1.mat');
 m = size(X, 1);
 
 % Randomly select 100 data points to display
-sel = randperm(size(X, 1));
-sel = sel(1:100);
+% sel = randperm(size(X, 1));
+% sel = sel(1:100);
+%
+% % Randomly select 100 data points to display
+rand_indices = randperm(m);
+sel = X(rand_indices(1:100), :);
 
-displayData(X(sel, :));
+displayData(sel);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ================ Part 2: Loading Pameters ================
-% In this part of the exercise, we load some pre-initialized 
+% In this part of the exercise, we load some pre-initialized
 % neural network parameters.
 
 fprintf('\nLoading Saved Neural Network Parameters ...\n')
@@ -61,6 +65,11 @@ load('ex3weights.mat');
 %  you compute the training set accuracy.
 
 pred = predict(Theta1, Theta2, X);
+for i=10:10:100
+myPred(i/10,:)=pred(rand_indices(i-10+1:i),:)';
+end
+myPred(myPred == 10) = 0;
+myPred
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 
@@ -73,16 +82,15 @@ pause;
 %  Randomly permute examples
 rp = randperm(m);
 
-for i = 1:m
-    % Display 
+for i = 1:5
+    % Display
     fprintf('\nDisplaying Example Image\n');
     displayData(X(rp(i), :));
 
     pred = predict(Theta1, Theta2, X(rp(i),:));
     fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
-    
+
     % Pause
     fprintf('Program paused. Press enter to continue.\n');
     pause;
 end
-
--- ex3/predict.m
+++ ex3/predict.m
@@ -7,12 +7,14 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+X = [ones(m,1) X];
+
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -20,7 +22,13 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+Z2 = X*Theta1';
+a2 = sigmoid(Z2);  % 5,000 x 25
+a2 = [ones(size(a2,1),1) a2];  % Add bias unit to a2 (column of 1's)
 
+Z3 = a2*Theta2';
+a3 = sigmoid(Z3);
+[maxProb,p] = max(a3,[],2);
 
 
 
