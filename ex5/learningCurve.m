eef0a3a9c5e3d9ea00e473ddb261e97fd180a8c4
dougwells <dougwells@gmail.com>
2016-04-01 17:03:51 -0600

    Wk6: Polynomial Feature Mapping Complete

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -1,11 +1,11 @@
 function [error_train, error_val] = ...
     learningCurve(X, y, Xval, yval, lambda)
-%LEARNINGCURVE Generates the train and cross validation set errors needed 
+%LEARNINGCURVE Generates the train and cross validation set errors needed
 %to plot a learning curve
 %   [error_train, error_val] = ...
 %       LEARNINGCURVE(X, y, Xval, yval, lambda) returns the train and
-%       cross validation set errors for a learning curve. In particular, 
-%       it returns two vectors of the same length - error_train and 
+%       cross validation set errors for a learning curve. In particular,
+%       it returns two vectors of the same length - error_train and
 %       error_val. Then, error_train(i) contains the training error for
 %       i examples (and similarly for error_val(i)).
 %
@@ -22,9 +22,9 @@ error_train = zeros(m, 1);
 error_val   = zeros(m, 1);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the cross validation errors in error_val. 
-%               i.e., error_train(i) and 
+% Instructions: Fill in this function to return training errors in
+%               error_train and the cross validation errors in error_val.
+%               i.e., error_train(i) and
 %               error_val(i) should give you the errors
 %               obtained after training on i examples.
 %
@@ -35,24 +35,58 @@ error_val   = zeros(m, 1);
 %       the _entire_ cross validation set (Xval and yval).
 %
 % Note: If you are using your cost function (linearRegCostFunction)
-%       to compute the training and cross validation error, you should 
-%       call the function with the lambda argument set to 0. 
+%       to compute the training and cross validation error, you should
+%       call the function with the lambda argument set to 0.
 %       Do note that you will still need to use lambda when running
 %       the training to obtain the theta parameters.
 %
 % Hint: You can loop over the examples with the following:
 %
 %       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
+%           % Compute train/cross validation errors using training examples
+%           % X(1:i, :) and y(1:i), storing the result in
 %           % error_train(i) and error_val(i)
 %           ....
-%           
+%
 %       end
 %
 
 % ---------------------- Sample Solution ----------------------
+% Dimension check
+% size(X)
+% size(y)
+% size(Xval)
+% size(yval)
 
+% Set lambda = 0 for training and validation
+lambdaTrain   = 0;  % No theta penalty in calculating cost function for Training
+lambdaVal     = 0;  % No theta penalty in calculating cost function for Cross Validation
+
+
+for i=1:m
+  [theta] = trainLinearReg(X(1:i,:), y(1:i,:), lambda);     % train new theta vector for each sample size(i).
+                                                            % NOTE:  Need full J (w/theta penalty) to find thetas
+
+  [Jtrain(i,:), grad] = linearRegCostFunction(X(1:i,:), y(1:i,:), theta, lambdaTrain);  % No theta penalty since thetas already found above
+  [Jval(i,:), grad] = linearRegCostFunction(Xval, yval, theta, lambdaVal);              % Run over full validation dataset each time
+end
+
+
+error_train = Jtrain;
+error_val = Jval;
+
+
+
+
+
+
+
+% for i=1:m
+% [J, grad] = linearRegCostFunction(X(1:i,:), y(i:i), theta, lambda);
+% J(i,:) = J;
+% theta = theta-grad
+%
+% end
 
 
 
--- ex5/polyFeatures.m
+++ ex5/polyFeatures.m
@@ -7,17 +7,19 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+m = size(X,1);
+X_poly = ones(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
+X_poly(:,1)=X_poly(:,1).*X(:,1);
 
-
-
-
+  for i=2:p
+    X_poly(:,i)=X_poly(:,i-1).*X(:,1);
+  end
 
 
 % =========================================================================
